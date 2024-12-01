4df8163bc7e403c19311e816c1334c018fa01f8a
matthew <matthew@melio.ai>
2024-12-01 12:35:46 +0200

    chore: solution to day 1

--- golang/2024/01/code.go
+++ golang/2024/01/code.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/jpillora/puzzler/harness/aoc"
+	"math"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -15,10 +20,114 @@ func main() {
 // 4. with: true (part2), and user input
 // the return value of each run is printed to stdout
 func run(part2 bool, input string) any {
+
 	// when you're ready to do part 2, remove this "not implemented" block
 	if part2 {
-		return "not implemented"
+		return solvePart2(input)
 	}
 	// solve part 1 here
-	return 42
+	return solvePart1(input)
+}
+
+func convertStrToInt(str string) int {
+	// string to int
+	i, err := strconv.Atoi(str)
+	if err != nil {
+		// ... handle error
+		panic(err)
+	}
+	return i
+}
+
+func extractInputPart1(input string) ([]int, []int) {
+	var list1 []int
+	var list2 []int
+
+	for _, row := range strings.Split(input, "\n") {
+		fields := strings.Fields(row)
+
+		list1 = append(list1, convertStrToInt(fields[0]))
+		list2 = append(list2, convertStrToInt(fields[1]))
+	}
+
+	sort.Slice(list1, func(i, j int) bool {
+		return list1[i] < list1[j]
+	})
+
+	sort.Slice(list2, func(i, j int) bool {
+		return list2[i] < list2[j]
+	})
+	return list1, list2
+}
+
+func calculateVariance(list_1 []int, list_2 []int) int {
+	var variances []int
+	totalVariance := 0
+	for i := 0; i < len(list_1); i++ {
+		variance := int(math.Abs(float64(list_1[i] - list_2[i])))
+		variances = append(variances, variance)
+		totalVariance += variance
+	}
+	//fmt.Printf("variances: %v\n", variances)
+	fmt.Printf("total variances: %v\n", totalVariance)
+	return totalVariance
+}
+
+func solvePart1(input string) any {
+	list1, list2 := extractInputPart1(input)
+	total_variance := calculateVariance(list1, list2)
+	return total_variance
+}
+
+func extractInputPart2(input string) ([]string, []string) {
+	var list1 []string
+	var list2 []string
+
+	for _, row := range strings.Split(input, "\n") {
+		fields := strings.Fields(row)
+
+		list1 = append(list1, fields[0])
+		list2 = append(list2, fields[1])
+	}
+	return list1, list2
+}
+
+func mapDuplicateCharacters(slice []string) map[string]int {
+	// Initialize an empty map to store counts
+	countMap := make(map[string]int)
+
+	// Iterate over the slice
+	for _, str := range slice {
+		// Increment the count for each string in the map
+		countMap[str]++
+	}
+	return countMap
+}
+
+func calculateSimilarity(slice1 []string, map2 map[string]int) int {
+	var similarities []int
+	var processedNumbers []string
+	totalSimilarities := 0
+
+	// Iterate through the map
+	for _, row := range slice1 {
+		processedNumbers = append(processedNumbers, row)
+
+		occurrencesInList2 := map2[row]
+		similarity := convertStrToInt(row) * occurrencesInList2
+
+		//fmt.Printf("key: %v, occurrencesInList2: %v, similarity: %v\n", row, occurrencesInList2, similarity)
+		similarities = append(similarities, similarity)
+		totalSimilarities += similarity
+	}
+	//fmt.Printf("variances: %v\n", variances)
+	fmt.Printf("total similarities: %v\n", totalSimilarities)
+	return totalSimilarities
+}
+
+func solvePart2(input string) any {
+	list1, list2 := extractInputPart2(input)
+	map2 := mapDuplicateCharacters(list2)
+	totalSimilarity := calculateSimilarity(list1, map2)
+	return totalSimilarity
 }
